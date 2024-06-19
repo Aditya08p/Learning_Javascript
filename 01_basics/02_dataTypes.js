dd2e5f98cc4fb66b144a4fea25c3572133f4fd01
Adi_tya21 <100029538+Deadshotxaddy@users.noreply.github.com>
2024-06-19 19:02:39 +0000

    data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version of js
+
+
+// alert("hello") // we are using nodejs, not browser thats why error
+
+console.log(3
+    +
+    3); //code readability should be high
+console.log("aditya");
+
+let name = "aditya"
+let age = 18
+let isLoggedIn = false
+let state // = null;
+// number => 2 to the power 53
+// bigint
+// string=> "   " 
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined type
+
