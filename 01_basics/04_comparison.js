984234cae5449564a1eb1151afa8ce4be2a3ad3d
Adi_tya21 <100029538+Deadshotxaddy@users.noreply.github.com>
2024-06-21 18:06:29 +0000

    comparison of different data types

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log(2 > "1");
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+ 
+//// comparisons and equality works differently in javascript
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+console.log("2" === 2); // strict check, also checks datatype
+
+// better to never compare different data types.
+// otherwise inconsistency in output is expected
