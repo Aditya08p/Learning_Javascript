bbcdee1461db290cd6576fb7573c0e75d6652c67
Adi_tya21 <100029538+Aditya08p@users.noreply.github.com>
2024-08-02 06:04:14 +0000

    fucntions in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("E");
+    console.log("L");
+    console.log("L");
+    console.log("O");
+}
+
+// sayMyName()
+
+function addTwoNum(number1 ,number2){
+    console.log(number1 + number2);
+}
+addTwoNum(2, 3)
