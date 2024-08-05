c4b13f1597654a1fa92b9e599b8d61b1b7f6f122
Adi_tya21 <100029538+Aditya08p@users.noreply.github.com>
2024-08-05 18:44:48 +0000

    functions in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,6 +10,21 @@ function sayMyName(){
 // sayMyName()
 
 function addTwoNum(number1 ,number2){
-    console.log(number1 + number2);
+    // console.log(number1 + number2);
+    
+    // let result = number1 + number2
+    // return result
+
+    return number1+number2
 }
-addTwoNum(2, 3)
+const result = addTwoNum(2, 3)
+// console.log("result:" , result);
+
+function loginusermsg(username ="sam"){ // overrided when given value, otherwise gives default value
+    if(!username){
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginusermsg("aditya patil"))
