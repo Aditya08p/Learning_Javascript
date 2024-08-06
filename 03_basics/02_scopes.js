1a276585fcf9f99e7736cca008ed74839babb0c2
Adi_tya21 <100029538+Aditya08p@users.noreply.github.com>
2024-08-06 04:44:17 +0000

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,6 +1,6 @@
 var c = 300
 let a = 300
-if(true){
+if(false){
     let a = 10
     const b = 20
     c = 30
@@ -8,6 +8,45 @@ if(true){
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "aditya"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    
+    two()
+
+}
+
+one()
+
+if (true) {
+    const username = "aditya"
+    if (username== "aditya") {
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+addone(5)
+function addone(num){
+    return num+1
+}
+
+// addTwo(5)   // cannot use expression functions before they are declared
+const addTwo = function(num){
+    return num+2
+}
