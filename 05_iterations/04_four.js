f8d92b709039b5445558c6746778537dd6d11a92
Adi_tya21 <100029538+Aditya08p@users.noreply.github.com>
2024-08-15 17:38:26 +0000

    more loops done

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,28 @@
+const myobject = {
+    js: "javascript",
+    cpp: "c++",
+    rb:"ruby",
+    swift: "swift by apple"
+}
+// for (const key in myobject) {
+//     console.log(`${key} shortcut is for ${myobject[key]}`);
+// }
+
+const programming = ["js","java","cpp","python"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+// }
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('FR',"France")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+
+// maps is not iteratable in js
